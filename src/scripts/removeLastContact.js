3525f13147ec3d735676346ab84660f4c26b4d68
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-06-17 17:52:06 +0300

    change task 7

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -5,14 +5,11 @@ export const removeLastContact = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
-    const newArray = [];
 
     if (contacts && contacts.length > 0) {
-      newArray.pop(contacts);
+      contacts.pop();
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts));
     }
-    // return contacts;
-
-    await fs.writeFile(PATH_DB, JSON.stringify());
   } catch (error) {
     console.log(error);
   }
